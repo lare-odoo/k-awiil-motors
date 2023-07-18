06896028f9b0f53a53d143d16f95ad34f0b7cc92
lare <lare@odoo.com>
2023-07-18 14:01:56 -0600

    "TE03: Add Models and Fields" finished

---
+++ KMMR/models/__init__.py
@@ -0,0 +1,3 @@
+# If there's a .py file, there
+# must be an init file
+from . import motorcycle
---
+++ KMMR/models/motorcycle.py
@@ -0,0 +1,17 @@
+from odoo import fields, models
+
+# Inherit from basic Model
+class Course(models.Model):
+    _name = 'motorcycle.registry'
+    _rec_name = 'registry_number'
+    
+    registry_number = fields.Char(string="Registry Number", required=True)
+    vin = fields.Char(required=True)
+    first_name = fields.Char(required=True)
+    last_name = fields.Char(required=True)
+    picture = fields.Image()
+    current_mileage = fields.Float()
+    license_plate = fields.Char()
+    certificate_title = fields.Boolean()
+    register_date = fields.Date()
+    
