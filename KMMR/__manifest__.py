ac1da344a891ce92d709bbd8226b305b4fe902bd
lare <lare@odoo.com>
2023-07-18 12:34:17 -0600

    "TE02: Create a Module" finished

---
+++ KMMR/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Motorcycle Registry",
+    'summary': "Manage Registration of Motorcycles",
+    'description': """Motorcycle Registry
+        ====================
+        This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.
+        """,
+    'author': 'lare-odoo',
+    'website': 'https://github.com/lare-odoo/k-awiil-motors',
+    'category': 'Kawiil/Kawiil',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'license': 'OPL-1',
+}
