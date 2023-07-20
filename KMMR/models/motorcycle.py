ad39f211d07eab0d8a53aab5daa66ee184bf253d
lare <lare@odoo.com>
2023-07-20 11:26:23 -0600

    "TE10: Advanced Fields" finished, fixed VIN regex bug, updated views for better image visualization

--- KMMR/models/motorcycle.py
+++ KMMR/models/motorcycle.py
@@ -18,9 +18,33 @@ class Course(models.Model):
         readonly=True,              # Users can't change this value
         )
 
+    # VIN format:
+    # -- Make - 2 capital letters
+    # -- Model - 2 capital letters
+    # -- Year - 2 digits
+    # -- Battery Capacity - 2 capital letters or digits
+    # -- Serial Number - 6 digits
     vin = fields.Char(string="VIN",required=True)
-    first_name = fields.Char(required=True)
-    last_name = fields.Char(required=True)
+
+    # Computed from VIN
+    make = fields.Char (compute = "_set_brand_make_model", readonly = "1")
+    model = fields.Char (compute = "_set_brand_make_model", readonly = "1")
+    year = fields.Char (compute = "_set_brand_make_model", readonly = "1")
+    
+    # We're using res.partner model to handle contact
+    # info, including name, phone and email.
+    # This requires a relationship field.
+
+    motorcycle_owner_id = fields.Many2one (
+        comodel_name = "res.partner", 
+        string = "Owner", 
+        ondelete = "restrict", 
+        required = True
+        )
+    
+    owner_phone = fields.Char (related = "motorcycle_owner_id.phone")
+    owner_email = fields.Char (related = "motorcycle_owner_id.email")
+
     picture = fields.Image()
     current_mileage = fields.Float()
     license_plate = fields.Char()
@@ -50,10 +74,27 @@ class Course(models.Model):
                 vals['registry_number'] = self.env['ir.sequence'].next_by_code('registry.number')
 
         return super().create(vals_list)
-    
+            
+    @api.constrains('license_plate')
+    def _check_license_plate (self):
+        regex = re.compile ("[A-Z]{1,4}[0-9]{1,3}([A-Z]{2})?$")
+
+        for motorcycle in self:
+            if regex.match(motorcycle.license_plate) == None:
+                raise ValidationError ('License plate doesn\'t match format')
+            
+    @api.constrains('vin')
+    def _check_registration_no(self):
+        for motorcycle in self:
+            domain = [('vin', '=', motorcycle.vin)]
+            count = self.sudo().search_count(domain)
+
+            if count > 1:
+                raise ValidationError ("VIN should be unique")
+            
     @api.constrains('vin')
     def _check_vin (self):
-        regex = re.compile ("[A-Z]{4}\d{2}([A-Z]{2}|\d{2})\d{6}")
+        regex = re.compile ("[A-Z]{4}\d{2}([A-Z]|\d){2}\d{6}")
 
         for motorcycle in self:
             if len(motorcycle.vin) != 14 or re.search(regex, motorcycle.vin) == None:
@@ -68,20 +109,10 @@ class Course(models.Model):
             if count > 1:
                 raise ValidationError ("VIN should be unique")
             '''
-            
-    @api.constrains('vin')
-    def _check_registration_no(self):
-        for motorcycle in self:
-            domain = [('vin', '=', motorcycle.vin)]
-            count = self.sudo().search_count(domain)
 
-            if count > 1:
-                raise ValidationError ("VIN should be unique")
-            
-    @api.constrains('license_plate')
-    def _check_vin (self):
-        regex = re.compile ("[A-Z]{1,4}[0-9]{1,3}([A-Z]{2})?$")
-
+    @api.depends ('vin')
+    def _set_brand_make_model (self):
         for motorcycle in self:
-            if regex.match(motorcycle.license_plate) == None:
-                raise ValidationError ('License plate doesn\'t match format')
+            motorcycle.make = motorcycle.vin[0:2]
+            motorcycle.model = motorcycle.vin[2:4]
+            motorcycle.year = motorcycle.vin[4:6]
